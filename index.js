e525beb5f942fd7139390f2c532c6f06879cd567
joaoooDias <joao_motta1@hotmail.com>
2025-06-03 21:25:06 -0300

    segundo dia

--- index.js
+++ index.js
@@ -18,9 +18,45 @@ botaoAdicionar.addEventListener("click", (evento) => {
     const nomeItem = document.createElement("p");
     nomeItem.innerText = inputItem.value;
 
+    inputCheckBox.addEventListener("click", function(){
+        if(inputCheckBox.checked) {
+            nomeItem.style.textDecoration = "line-through";
+        } else {
+            nomeItem.style.textDecoration = "none"
+        }
+    })
+
+
     containerItemDaLista.appendChild(inputCheckBox);
     containerItemDaLista.appendChild(nomeItem);
 
     itemDaLista.appendChild(containerItemDaLista);
+
+    const diaDaSemana = new Date().toLocaleDateString("pt-BR", {weekday:"long"});
+    const data = new Date().toLocaleDateString("pt-BR")
+    const hora = new Date().toLocaleTimeString("pt-BR",{
+        hour: "numeric",
+        minute: "numeric"
+    })
+    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`;
+    const itemData = document.createElement("p");
+    itemData.innerText = dataCompleta;
+    itemData.classList.add("texto-data")
+    itemDaLista.appendChild(itemData)
     listaDeCompras.appendChild(itemDaLista);
+
+    verificarListaVazia(); 
 })
+
+const mensagemListaVazia = document.querySelector(".mensagem-lista-vazia");
+
+function verificarListaVazia(){
+    const itensDaLista = listaDeCompras.querySelectorAll("li");
+    if (itensDaLista.length === 0){
+        mensagemListaVazia.style.display = "block"
+    } else{
+        mensagemListaVazia.style.display = "none"
+    }
+}
+
+verificarListaVazia();
